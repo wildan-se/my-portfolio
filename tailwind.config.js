4088d0c3bb749db855daa80b2077a2d74b48d049
Wl.se <muhammad.wildan@raharja.info>
2025-09-29 13:40:33 +0700

    fix foto di hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
-content: ["./**/*.html"],
-  (module.exports = {
-    content: ["index.html"],
-    darkMode: "class",
-    theme: {
-      container: {
-        center: true,
-        padding: "16px",
+module.exports = {
+  content: ["./**/*.html"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "rgb(110 231 183)",
+        secondary: "#64748b",
+        dark: "#0f172a",
       },
-      extend: {
-        colors: {
-          primary: "rgb(110 231 183)",
-          secondary: "#64748b",
-          dark: "#0f172a",
-        },
-        screens: {
-          "2xl": "1320px",
-        },
+      screens: {
+        "2xl": "1320px",
       },
     },
-    plugins: [],
-  });
+  },
+  plugins: [],
+};
